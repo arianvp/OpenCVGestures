b2a959bcd066218156ad5eb0e4f5380b164ad5dc
BaseKan <Bas_de_kan@live.nl>
2015-03-17 11:39:28 +0100

    je moeder

--- gest.py
+++ gest.py
@@ -39,32 +39,28 @@ def captureBG():
     while True:
         ret,frame = cap.read()
         frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        frame = cv2.GaussianBlur(frame,(31,31),0)
+        frame = cv2.GaussianBlur(frame,(5,5),0)
         cv2.imshow('Capture background',frame)
         if cv2.waitKey(1) & 0xFF == ord('c'):
             return frame
 
-
-def applyThreshold(frame, threshold):
-    def f(x):
-        if x > threshold:
-            return (255,255,255)
-        else:
-            return (0,0,0)
-    return map(f,frame)
 def bgSubtractor():
     bg = captureBG()
-
+    dkernel = np.ones((15,15), np.uint8)
+    ekernel = np.ones((15,15),np.uint8)
     while cap.isOpened:
         ret,frame = cap.read()
         frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         frame = cv2.absdiff(bg,frame)
-        ret,frame = cv2.threshold(frame, 25,255, cv2.THRESH_BINARY)
-        frame = cv2.dilate(frame,None,20)
-        frame = cv2.erode(frame,None,20)
+        cv2.imshow('jemoeder',frame)
+        ret,frame = cv2.threshold(frame, 30, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
         cv2.imshow('frame', frame)
+        frame = cv2.erode(frame,ekernel,3)
+        cv2.imshow('frameErode', frame)
+        frame = cv2.dilate(frame,dkernel,3)
+        cv2.imshow('frameDilate', frame)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
 
 bgSubtractor()
